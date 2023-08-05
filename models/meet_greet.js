deb6ce05ddac8a4ba2c16c44210332e1c760820d
Cristian Nambo <cristian-nambo@hotmail.com>
2023-08-05 02:46:37 -0700

    Created controllers

---
+++ models/meet_greet.js
@@ -0,0 +1,41 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Meet_Greet extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Meet_Greet.init({
+    meet_greet_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    available_start_time: {
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+    end_time: {
+        type: DataTypes.DATE,
+        allowNull: false
+    }
+  }, {
+    sequelize,
+    modelName: 'Meet_Greet',
+    tableName: 'meet_greet',
+    timestamps: false
+  });
+  return Meet_Greet;
+};
